d1758eb2f9228131310a2f8be7ee78a677282375
biwen <biwen@kth.se>
2017-11-15 16:19:41 +0100

    the update version

---
+++ dijkstras_rule_engine/Graph.java
@@ -0,0 +1,27 @@
+package dijkstra;
+
+/**
+ *
+ * @author biwen
+ */
+import java.util.List;
+
+public class Graph {
+    private final List<Vertex> vertexes;
+    private final List<Edge> edges;
+    
+    public Graph(List<Vertex> vertexes, List<Edge> edges) {
+        this.vertexes = vertexes;
+        this.edges = edges;
+    }
+    
+    public List<Vertex> getVertexes() {
+        return vertexes;
+    }
+    
+    public List<Edge> getEdges() {
+        return edges;
+    }
+    
+}
+
