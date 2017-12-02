48f4aee8cc0d54ff7df68a6473d0b77d813decfa
iaq <iaq@kth.se>
2017-12-02 15:03:37 +0100

    chnaged main and graph

---
+++ FinalDijkstra/DijkstraTestStack/src/readJson.java
@@ -0,0 +1,69 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class readJson {
+    public static void main(String[] args) throws Exception {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject;
+        JSONArray nodes;
+        JSONArray links;
+        Map<String, Graph.Vertex> vertices = new HashMap<>();
+
+        try {
+            jsonObject = (JSONObject) parser.parse(new FileReader("C:\\Users\\ibrah\\Desktop\\UunetTest.json"));
+            // save the results from reading nodes and links into a JSON arrays
+            nodes = (JSONArray) jsonObject.get("nodes");
+            links = (JSONArray) jsonObject.get("links");
+            // create a map that has id as a key and json object
+            Map<String, JSONObject> mappedNodes = new HashMap<String, JSONObject>();
+            for (Object tmpNode : nodes) {
+                JSONObject tmpNode1 = (JSONObject) tmpNode;
+                mappedNodes.put(tmpNode1.get("id").toString(), tmpNode1);
+
+            }
+
+            for (Object linkObj : links) {
+                JSONObject link = (JSONObject) linkObj;
+                JSONObject source = mappedNodes.get(link.get("source").toString());
+                JSONObject target = mappedNodes.get(link.get("target").toString());
+                double srcLatitude = Double.parseDouble(source.get("Latitude").toString());
+                double srcLongitude = Double.parseDouble(source.get("Longitude").toString());
+                double dstLatitude = Double.parseDouble(target.get("Latitude").toString());
+                double dstLongitude = Double.parseDouble(target.get("Longitude").toString());
+                double distance = getDistance(srcLatitude, srcLongitude, dstLatitude, dstLongitude);
+                System.out.println("Distance between " + " source " + source.get("id") + "----- and destination -----> "
+                        + target.get("id") + " ------ " + distance);
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static double getDistance(double lat1, double lon1, double lat2, double lon2) {
+        double theta = lon1 - lon2;
+        double dist = Math.sin(degreeToRadiens(lat1)) * Math.sin(degreeToRadiens(lat2)) + Math.cos(degreeToRadiens(lat1)) * Math.cos(degreeToRadiens(lat2)) * Math.cos(degreeToRadiens(theta));
+        dist = Math.acos(dist);
+        dist = radiensTodegrees(dist);
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344;
+        return (dist);
+    }
+
+    private static double degreeToRadiens(double degree) {
+        return (degree * Math.PI / 180.0);
+    }
+
+    private static double radiensTodegrees(double radines) {
+        return (radines * 180 / Math.PI);
+    }
+}
--- FinalDijkstra/DijkstraTestStack/src/testwrite.java
+++ FinalDijkstra/DijkstraTestStack/src/testwrite.java
@@ -9,18 +9,11 @@ public class testwrite {
     @SuppressWarnings("unchecked")
     public static void main(String[] args) {
 
-        // JSONObject countryObj = new JSONObject();
-        // countryObj.put("Name", "India");
-        //countryObj.put("Population", new Integer(1000000));
-
-        //JSONArray listOfStates = new JSONArray();
-        //listOfStates.add("Madhya Pradesh");
-        //listOfStates.add("Maharastra");
-        //listOfStates.add("Rajasthan");
-
-        //countryObj.put("States", listOfStates);
         JSONArray jsonArray = new JSONArray();
         JSONObject jsonObj = new JSONObject();
+        JSONObject mainObj = new JSONObject();
+        mainObj.put("links", jsonArray);
+
 
         for (int i = 0; i < 10; i++) {
 
@@ -28,25 +21,23 @@ public class testwrite {
             jsonObj.put("target", i+1);
             jsonArray.add(jsonObj);
 
-            JSONObject mainObj = new JSONObject();
-            mainObj.put("links", jsonArray);
-            try {
+        }
 
-                // Writing to a file
-                FileWriter file = new FileWriter("Testtest.json");
-               file.append(mainObj.toJSONString());
-               // FileWriter file = new FileWriter(file);
-                System.out.println();
-                System.out.print(mainObj.toJSONString());
+        try {
 
-              //  file.write(jsonArray.toJSONString());
-                file.flush();
-               file.close();
+            // Writing to a file
+            FileWriter file = new FileWriter("Testtest.json");
+            file.append(mainObj.toJSONString());
+            // FileWriter file = new FileWriter(file);
+            System.out.println();
+            System.out.print(mainObj.toJSONString());
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            //  file.write(jsonArray.toJSONString());
+            file.flush();
+            file.close();
 
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
