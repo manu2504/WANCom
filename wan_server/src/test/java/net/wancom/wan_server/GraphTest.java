d621a19750fc95ce12d588d5d75b40505cc75f8e
Farzad Hajibagheri <farzadha@kth.se>
2017-12-05 22:06:45 +0100

    Preparation of test class to test the Graph class (Graph improvement's algorithm).

---
+++ wan_server/src/test/java/net/wancom/wan_server/GraphTest.java
@@ -0,0 +1,30 @@
+package net.wancom.wan_server;
+
+import org.junit.Test;
+
+public class GraphTest {
+
+    /*
+    * [links]
+    * [vertex]
+    * Input (graph, src, dst, cost_limit)
+    * Output (graph, cost, latency_reduction)
+    */
+
+
+    @Test
+    void calculateCostEfficiencyOfImprovedGraph() {
+        // We are going to get the total cost of graph improvement.
+    }
+
+    @Test
+    void calculateLatencyReductionOfImprovedGraph() {
+        //calculation of the latency reduction will be here.
+    }
+
+    @Test
+    void calculateComputationTimeOfGraphImprovement() {
+        //calculation of computation time when the system improve the graph.
+    }
+
+}
