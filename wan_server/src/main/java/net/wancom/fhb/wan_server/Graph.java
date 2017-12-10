601ef91cb99c829b811102f9946ac1a36e2844af
Farzad Hajibagheri <farzadha@kth.se>
2017-12-10 16:20:12 +0100

    Graph class that is a set of Nodes

---
+++ wan_server/src/main/java/net/wancom/fhb/wan_server/Graph.java
@@ -0,0 +1,34 @@
+package net.wancom.fhb.wan_server;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * We have a Graph that is a set of nodes.
+ * @see {@link Node}
+ */
+public class Graph {
+
+    private Set<Node> nodes;
+
+    private Graph(){
+        this.nodes = new HashSet<>();
+    }
+
+    public static Graph initGraph() {
+        return new Graph();
+    }
+
+    public void addNode(Node node) {
+        nodes.add(node);
+    }
+
+    // getters and setters
+    public Set<Node> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(Set<Node> nodes) {
+        this.nodes = nodes;
+    }
+}
