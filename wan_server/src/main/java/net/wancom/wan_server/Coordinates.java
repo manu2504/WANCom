7b6c1bce105e9b59b2e0825010134de192db2661
chaudron <chaudron@kth.se>
2017-12-12 19:36:11 +0100

    Added a class to represent Coordinates

---
+++ wan_server/src/main/java/net/wancom/wan_server/Coordinates.java
@@ -0,0 +1,12 @@
+package net.wancom.wan_server;
+
+public class Coordinates {
+
+  public double lat;
+  public double lng;
+  
+  public Coordinates(double lat, double lng) {
+    this.lat = lat;
+    this.lng = lng;
+  }
+}
