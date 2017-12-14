76e5f1629c09d2e95d2cad90581cfb7038ca45f9
Farzad Hajibagheri <farzadha@kth.se>
2017-12-14 14:02:16 +0100

    Costumize exception class for a better exception handling.

---
+++ wan_server/src/main/java/net/wancom/exceptions/WanComException.java
@@ -0,0 +1,10 @@
+package net.wancom.exceptions;
+
+/**
+ * Created by farzadha@kth.se on 2017-11-01.
+ */
+public class WanComException extends RuntimeException {
+    public WanComException(String message) {
+        super(message);
+    }
+}
