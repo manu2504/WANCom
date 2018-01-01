95626c04948dc49e4bf12d7aa228596fc7a57ba5
iaq <iaq@kth.se>
2018-01-01 16:51:46 +0100

    implementing the new algorithm in NewGraph.java and made some modification in class Graph and JSONUtils

--- wan_server/src/main/java/net/wancom/eics/NewGraph.java
+++ wan_server/src/main/java/net/wancom/eics/NewGraph.java
@@ -2,9 +2,11 @@ package net.wancom.eics;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import net.wancom.wan_server.SphericalGeometry;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
@@ -21,29 +23,52 @@ public class NewGraph {
    * the new total distance for the modified graph. This performance should be stored in a variable
    * that stores the best performance, and be replaced every time a better performance is found.
    */
-  
-  public Graph addBestNewNode(Graph graph, String country, int constraint) throws FileNotFoundException, IOException {
-    JSONObject jsonObject = JSONUtils.JSONObjectFromJSONFile(country);
-    JSONArray nodesList = (JSONArray) jsonObject.get("nodes");
-    Map<String, Node> mapNodes = new HashMap<>();//not necessary 
-    for (int i=0; i<nodesList.size();i++){
-      JSONObject nodeObject = (JSONObject) nodesList.get(i);
-      Node node = new Node("this node"+i);
-      double nodeLat = Double.parseDouble(nodeObject.get("Latitude").toString());
-      double nodeLong= Double.parseDouble(nodeObject.get("Longitude").toString());
-      node.setLatitude(nodeLat);
-      node.setLongitude(nodeLong);
-      mapNodes.put("this node" +i,node); //this might not be necessary
-      graph.addNode(node); // this means that every nodes is in the graph
 
-      //TODO: test each individual node with the preexisting nodes in the graph and calculate the distance between them
-      //TODO:
+    public static Graph addBestNewNode(Graph graph, String country, int constraint) throws FileNotFoundException, IOException {
+        JSONObject jsonObject = JSONUtils.JSONObjectFromJSONFile(country);
+        ArrayList<Node> oldnodes;
+        JSONArray nodesList = (JSONArray) jsonObject.get("nodes");
+        Map<String, Node> mapNodes = new HashMap<>();//not necessary
+        oldnodes = (ArrayList<Node>) graph.getNodes();
+        for (int i=0; i<nodesList.size();i++){
+            JSONObject nodeObject = (JSONObject) nodesList.get(i);
+            Node newNode = new Node("this newNode"+i);
+            double nodeLat = Double.parseDouble(nodeObject.get("Latitude").toString());
+            double nodeLong= Double.parseDouble(nodeObject.get("Longitude").toString());
+            newNode.setLatitude(nodeLat);
+            newNode.setLongitude(nodeLong);
+            mapNodes.put("newNode" +i,newNode); //this might not be necessary
+            graph.addNode(newNode); // this means that every nodes is in the graph
 
+            for (int j =0; j<oldnodes.size();j++){
+                Node sourceNode = oldnodes.get(j);
+                int cost = SphericalGeometry.getDistance(sourceNode.getLatitude(), sourceNode.getLongitude(),
+                        newNode.getLatitude(), newNode.getLongitude());
+                sourceNode.addImmediateNeighborsDestination(newNode, cost);
+                newNode.addImmediateNeighborsDestination(sourceNode, cost);
+                for(int x=j+1;x<oldnodes.size();x++){
+                    Node targetNode = oldnodes.get(j);
+                    int cost2 = SphericalGeometry.getDistance(targetNode.getLatitude(), targetNode.getLongitude(),
+                            newNode.getLatitude(), newNode.getLongitude());
+                    targetNode.addImmediateNeighborsDestination(newNode, cost2);
+                    newNode.addImmediateNeighborsDestination(targetNode, cost2);
+                    int totalCost= cost+cost2;
+                    if(totalCost>constraint){
+                            continue;
+                    }
+
+                   for (int y=0;y<oldnodes.size();x++){
+                       Dijkstra.calculateShortestPathFromSource(graph, sourceNode);
+                   }
+                   return graph;
+
+
+                }
+
+
+            }
+        }
+        return Graph.initGraph();
     }
-
-    // TODO
-    
-    return Graph.initGraph();
-  }
 
 }
